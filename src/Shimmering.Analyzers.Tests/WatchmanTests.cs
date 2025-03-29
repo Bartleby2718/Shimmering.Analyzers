e134ce9ff63b475f82b2f0d8a90754b54179759e
Jihoon Park <bartleby2718@gmail.com>
2025-03-28 22:48:00 -0400

    Close #75: Add a test to ensure that AllRules.md is up to date

--- src/Shimmering.Analyzers.Tests/WatchmanTests.cs
+++ src/Shimmering.Analyzers.Tests/WatchmanTests.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 
 namespace Shimmering.Analyzers.Tests;
 
+[Parallelizable]
 public class WatchmanTests
 {
 	[Test]
@@ -93,4 +94,90 @@ public class WatchmanTests
 
 		Assert.That(existingMarkdownFiles, Is.EquivalentTo(expectedMarkdownFiles));
 	}
+
+	[Test]
+	public void TestAllRulesTableIsFormattedCorrectly()
+	{
+		var analyzers = GetAnalyzers();
+
+		// sort by Id in ascending order
+		analyzers.Sort((a1, a2) => a1.Id.CompareTo(a2.Id));
+
+		var allRulesFile = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "docs", "AllRules.md"));
+		var lines = File.ReadAllLines(allRulesFile)
+			.SkipWhile(line => !line.StartsWith("# Table"))
+			.ToArray();
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(lines[0], Is.EqualTo("# Table"));
+			Assert.That(lines[1], Is.EqualTo("  Rule ID   | Category | Severity | Since | Notes"));
+			Assert.That(lines[2], Is.EqualTo("------------|----------|----------|-------|-------"));
+			Assert.That(lines, Has.Length.EqualTo(analyzers.Count + 3));
+		});
+
+		Assert.Multiple(() =>
+		{
+			for (int i = 3; i < lines.Length; ++i)
+			{
+				Assert.DoesNotThrow(
+					() => Assert.Multiple(() =>
+						{
+							var line = lines[i];
+							var columns = line.Split("|");
+							Assert.That(columns, Has.Length.EqualTo(5));
+
+							var analyzer = analyzers[i - 3];
+
+							Assert.That(columns[0], Is.EqualTo($"{analyzer.Id} "));
+
+							Assert.That(columns[1], Is.EqualTo($"  {analyzer.Category}   "));
+
+							var severity = analyzer.Severity.ToString();
+							Assert.That(columns[2], Is.EqualTo(CenterAlign(severity, totalWidth: 10)));
+
+							// skip validation on the Since column
+							var link = $"[Documentation]({analyzer.Category}Rules/{analyzer.Id}.md)";
+							Assert.That(columns[4], Is.EqualTo($" {analyzer.Name}, {link}"));
+						}),
+					message: lines[i]);
+			}
+		});
+	}
+
+	private static List<AnalyzerInfo> GetAnalyzers()
+	{
+		var analyzerTypes = typeof(ShimmeringAnalyzer).Assembly
+			.GetTypes()
+			.Where(t => t.IsClass
+				&& !t.IsAbstract
+				&& typeof(ShimmeringAnalyzer).IsAssignableFrom(t))
+			.ToArray();
+
+		List<AnalyzerInfo> analyzers = [];
+		foreach (var type in analyzerTypes)
+		{
+			if (Activator.CreateInstance(type) is not ShimmeringAnalyzer instance) { continue; }
+
+			// So far, all analyzers support a single diagnostic each.
+			Assert.That(instance.SupportedDiagnostics, Has.Length.EqualTo(1));
+
+			var descriptor = instance.SupportedDiagnostics.Single();
+			var analyzerName = instance.GetType().Name;
+			analyzers.Add(new(descriptor.Id, analyzerName, descriptor.Category, descriptor.DefaultSeverity));
+		}
+
+		return analyzers;
+	}
+
+	private static string CenterAlign(string text, int totalWidth)
+	{
+		int totalPadding = totalWidth - text.Length;
+		int leftPadding = totalPadding / 2;  // Floor division
+		int rightPadding = totalPadding - leftPadding; // The rest goes to the right
+
+		return new string(' ', leftPadding) + text + new string(' ', rightPadding);
+	}
+
+	private record AnalyzerInfo(string Id, string Name, string Category, DiagnosticSeverity Severity);
 }
