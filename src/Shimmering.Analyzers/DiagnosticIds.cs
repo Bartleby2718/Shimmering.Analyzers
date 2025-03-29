1ad878dc0d3d2d01f6e9e81d55512e510f5c2464
Jihoon Park <bartleby2718@gmail.com>
2025-03-29 18:13:27 -0400

    Add comments about upcoming work

--- src/Shimmering.Analyzers/DiagnosticIds.cs
+++ src/Shimmering.Analyzers/DiagnosticIds.cs
@@ -11,11 +11,14 @@ internal static class DiagnosticIds
 		public const string ToListForEach = "SHIMMER1010";
 		public const string ToArrayOrToListFollowedByLinqMethod = "SHIMMER1011";
 		public const string ArrayOrArrayReturningMethodFollowedByToArray = "SHIMMER1012";
+		// TODO: 1013 = https://github.com/Bartleby2718/Shimmering.Analyzers/issues/49
+		// TODO: 1014 = https://github.com/Bartleby2718/Shimmering.Analyzers/issues/50
 		public const string SingleUseIEnumerableMaterialization = "SHIMMER1015";
 		// 102X: collection expression
 		public const string RedundantSpreadElement = "SHIMMER1020";
-		// 104X: string.Split()
+		// 103X: string.Split()
 		public const string MissingRemoveEmptyEntries = "SHIMMER1030";
+		// TODO: 1031 = https://github.com/Bartleby2718/Shimmering.Analyzers/issues/
 
 		// 110X: idiomatic use of LINQ methods
 		public const string MisusedOrDefault = "SHIMMER1100";
