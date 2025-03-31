6f5d944a15eb9c720bbb4d8cde1b32519c02a5f6
Jihoon Park <bartleby2718@gmail.com>
2025-03-30 22:28:51 -0400

    Fix editUrl

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Bartleby2718/Shimmering.Analyzers',
+            'https://github.com/Bartleby2718/Shimmering.Analyzers/blob/main/site/',
         },
         blog: false, // Disable blog
         theme: {
